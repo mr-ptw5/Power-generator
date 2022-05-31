e64fe7344eb145a1a98331342b5dc78975f2dd96
mr-ptw5 <wrightptw@gmail.com>
2022-05-31 17:30:13 -0500

    adding flair

--- js/main.js
+++ js/main.js
@@ -1,27 +1,64 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
-
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-console.log('testing')
+const wordBox = document.querySelector('section h1')
 const btn = document.querySelector("button"); // Get the button from the page
 // Detect clicks on the button
 if (btn) {
-  btn.onclick = async function() {
-    const res = await fetch(`/api`)
-    const data = await res.json()
-    
-    console.log('testing!')
-    document.querySelector('h2').textContent = data.superpower
+    btn.onclick = async function () {
+        const res = await fetch(`/api`)
+        const data = await res.json()
+
+        bestow(data.superpower, data.url)
+    };
+}
+
+const take = document.querySelector('#take')
+const leave = document.querySelector('#leave')
+
+take.addEventListener("click", () => {
+    take.style.display = 'none'
+    leave.style.display = 'none'
+    document.querySelector('h2').style.display = 'none'
+    let remarks = ["COOL", "NEAT"]
+    let i = Math.round(Math.random())
+    document.querySelector('h3').innerText = remarks[i]
+    setTimeout(() => {
+        document.querySelector('h3').innerText = ""
+    }, 750)
+})
+
+leave.addEventListener("click", () => {
+    take.style.display = 'none'
+    leave.style.display = 'none'
+    document.querySelector('h2').style.display = 'none'
+    let remarks = ["OK FINE THEN.", "...OK."]
+    let i = Math.round(Math.random())
+    document.querySelector('h3').innerText = remarks[i]
+    setTimeout(() => {
+        document.querySelector('h3').innerText = ""
+    }, 750)
+})
+
+
+function bestow(superpower, url) {
     const link = document.querySelector('a')
-    link.href = data.url
-    link.style.display = 'inline'
-  };
+    const snark = document.querySelector('h2')
+
+    link.style.display = 'none'
+    snark.style.display = 'none'
+    take.style.display = 'none'
+    leave.style.display = 'none'
+    wordBox.textContent = 'I'
+    setTimeout(() => {
+        wordBox.textContent = 'GRANT'
+        setTimeout(() => {
+            wordBox.textContent = 'YOU'
+            setTimeout(() => {
+                wordBox.textContent = superpower
+                link.href = url
+                link.style.display = 'block'
+                snark.style.display = 'block'
+                take.style.display = 'inline'
+                leave.style.display = 'inline'
+            }, 700)
+        }, 250)
+    }, 250)
 }
